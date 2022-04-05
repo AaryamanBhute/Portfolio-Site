a4b3498cabfff196112dca8749f1ede0fa88156c
AaryamanBhute <aryamanbhute@gmail.com>
2022-04-04 23:27:07 -0700

    updates

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-picdc&@oi=^q5$p%+c@y4**h!jqe%&h&up6$nz+o7dzc#3)f+('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
