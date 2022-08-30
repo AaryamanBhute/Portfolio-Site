8bf03b95fd3c36bf4ef6b32fb9be78bb8515425e
Aaryaman Bhute <aaryaman.bhute@team.neustar>
2022-08-30 15:59:01 +0530

    force https

--- personalsite/settings.py
+++ personalsite/settings.py
@@ -26,6 +26,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'temp')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 ALLOWED_HOSTS = ['*']
 
